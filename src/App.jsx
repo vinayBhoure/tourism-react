b089a1f2354c4753680f18c3d341c70421d15ad0
btp-2024 <165655409+btp-2024@users.noreply.github.com>
2024-04-02 15:38:32 +0530

    add styling

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import AboutUs from "./scenes/aboutUs";
+import Attractions from "./scenes/attractions";
+import HomePage from "./scenes/homePage";
+import Contact from "./scenes/contact";
+import Mice from "./scenes/mice";
+import Packages from "./scenes/packages";
+import VehicleFleet from "./scenes/vehicleFleet";
+import Terms from "./scenes/terms";
+import Privacy from "./scenes/privacy";
+
+import { LoginPage } from "./scenes/login/page";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Copyright from "./components/Copyright";
+import { SignUpPage } from "./scenes/signup/page";
+
+
+
+function App() {
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/about" element={<AboutUs />} />
+					<Route path="/attractions" element={<Attractions />} />
+					<Route path="/contact" element={<Contact />} />
+					{/* <Route path="/mice" element={<Mice />} /> */}
+					<Route path="/packages" element={<Packages />} />
+					<Route path="/vehiclefleet" element={<VehicleFleet />} />
+					<Route path="/packages" element={<Packages />} />
+					<Route path="/terms" element={<Terms />} />
+					<Route path="/privacy" element={<Privacy />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/signup" element={<SignUpPage />} />
+				</Routes>
+				<Footer />
+				<Copyright />
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './main_styles.css'
+import './responsive.css'
+import './animate.css'
+import "./App.css";
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './owl.corousel.css'
+import './owl.theme.default.css'
+import App from './App';
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './api/query-client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>,
+)
+
